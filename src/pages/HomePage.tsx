c3042481f84191b9bf781d8c3134e3ea97aa5fb1
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-07-14 02:25:04 +0000

    Bob AI: add the functionality and interface elements for selling boo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,20 +7,20 @@
 import React from 'react';
 import HeroSection from './HomePage/sections/Hero';
 import AboutSection from './HomePage/sections/About';
-import ProductsSection from './HomePage/sections/Products';
 import MetricsSection from './HomePage/sections/Metrics';
 import TestimonialSection from './HomePage/sections/Testimonial';
 import FaqSection from './HomePage/sections/Faq';
 import ContactSection from './HomePage/sections/Contact';
 
-export default function HomePage(): React.JSX.Element {
+
+import BookStoreSection from './HomePage/sections/BookStore';export default function HomePage(): React.JSX.Element {
   return (
 <>
   <HeroSection />
 
   <AboutSection />
+      <BookStoreSection />
 
-  <ProductsSection />
 
   <MetricsSection />
 
